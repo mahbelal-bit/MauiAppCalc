4cb3d5a854c58b212f19094e1671fa4921043f01
Mahajna Belal <mahbelal@hotmail.com>
2025-07-07 19:20:47 +0300

    1

--- MauiApp2/MainPage.xaml.cs
+++ MauiApp2/MainPage.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Linq.Expressions;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MauiApp2
 {
